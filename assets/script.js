c2c2a77c4411fd84b61265fdb45eb457f4a03bff
lpakingan <lpak98@gmail.com>
2023-05-05 11:35:39 -0700

    add findCoordinates function for finding coordinates of input city

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,6 @@
 var APIKey = '9b79a9a79e9cfce63f28b86f4b366b67';
 var submitButton = document.getElementById('submit-button');
 
-var cityInput;
-// to obtain the coordinates from the input city
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-var coordinateQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${APIKey}` 
-
 var currentLat;
 var currentLon;
 // to get weather from coordinates pulled from the coordinateQuery API
@@ -13,12 +8,12 @@ var currentLon;
 var queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${APIKey}`
 
 function getCity() {
-    // var searchQuery = document.location.search.split('&')
-    var searchQuery = coordinateQuery.split('&');
+    var searchQuery = document.location.search.split('&')
+    // var searchQuery = coordinateQuery.split('&');
 
     var city = searchQuery[0].split('=').pop();
     console.log(city)
-}
+};
 
 function handleCityInput(event) {
     event.preventDefault();
@@ -29,9 +24,22 @@ function handleCityInput(event) {
         alert('You must enter a city!');
         return;
     }
+    
+    findCoordinates(cityInput)
+};
 
-    getCity()
-    console.log(cityInput)
-}
+function findCoordinates(cityInput) {
+    // to obtain the coordinates from the input city
+    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    var coordinateQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${APIKey}` 
+
+    fetch(coordinateQuery).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    });
+};
 
 submitButton.addEventListener('click', handleCityInput)
