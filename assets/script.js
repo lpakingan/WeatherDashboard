63751e7f988753c3b2c36c0affa53928827c675c
lpakingan <lpak98@gmail.com>
2023-05-05 10:28:28 -0700

    update variables for pulling information from API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 var APIKey = '9b79a9a79e9cfce63f28b86f4b366b67';
+
+var cityName;
+var cityOptions;
+// to obtain the coordinates from the input city
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var coordinateQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${cityOptions}&appid=${APIKey}` 
+
 var currentLat;
 var currentLon;
-
+// to get weather from coordinates pulled from the coordinateQuery API
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-var queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${APIKey}` 
+var queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${APIKey}`
